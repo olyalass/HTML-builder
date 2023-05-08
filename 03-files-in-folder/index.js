64a4295c01c19d75fd65219f4487e68c3d1fa430
olyalass <olyalass@icloud.com>
2023-05-08 18:11:56 +0200

    feat: complete task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const filesys = require("fs");
+const path = require("path");
+
+const dir = path.join(__dirname, "./secret-folder");
+
+filesys.readdir(dir, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error(`Error: ${err}`);
+    return;
+  }
+
+  files
+    .filter((e) => e.isFile())
+    .forEach((e) => {
+      const filePath = path.join(dir, e.name);
+      const stats = filesys.statSync(filePath);
+      const ext = path.extname(filePath);
+      const size = stats.size;
+      console.log(`${e.name} - ${ext} - ${size} bytes`);
+    });
+});
