4202c5d535e939ce9a1306eca93ca4a9fc26afcc
olyalass <olyalass@icloud.com>
2023-05-08 21:42:38 +0200

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,64 @@
+const filesys = require("fs");
+const path = require("path");
+const source = path.join(__dirname, "./files");
+const dest = path.join(__dirname, "./files-copy");
 
+function copyDir(source, dest) {
+  if (doesExist(dest)) {
+    filesys.rm(
+      dest,
+      {
+        recursive: true,
+        force: true,
+      },
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        copyDir(source, dest);
+      }
+    );
+  } else {
+    filesys.mkdir(dest, { recursive: true }, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      filesys.readdir(source, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          throw err;
+        }
+
+        files.forEach((e) => {
+          const sourceFile = path.join(source, e.name);
+          const destFile = path.join(dest, e.name);
+
+          if (e.isDirectory()) {
+            copyDir(sourceFile, destFile);
+          } else {
+            filesys.copyFile(sourceFile, destFile, (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+          }
+        });
+      });
+    });
+  }
+}
+
+function doesExist(path) {
+  try {
+    const stats = filesys.statSync(path);
+    return stats.isDirectory();
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      return false;
+    } else {
+      throw err;
+    }
+  }
+}
+
+copyDir(source, dest);
