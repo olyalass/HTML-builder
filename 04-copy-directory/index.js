b7cb377ddf1561f62df8236beaf2c87f6f1869b2
olyalass <olyalass@icloud.com>
2023-05-09 15:20:09 +0200

    fix: get rid of sync method in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,61 +4,64 @@ const source = path.join(__dirname, "./files");
 const dest = path.join(__dirname, "./files-copy");
 
 function copyDir(source, dest) {
-  if (doesExist(dest)) {
-    filesys.rm(
-      dest,
-      {
-        recursive: true,
-        force: true,
-      },
-      (err) => {
-        if (err) {
-          throw err;
-        }
-        copyDir(source, dest);
-      }
-    );
-  } else {
-    filesys.mkdir(dest, { recursive: true }, (err) => {
-      if (err) {
-        throw err;
-      }
-
-      filesys.readdir(source, { withFileTypes: true }, (err, files) => {
-        if (err) {
-          throw err;
-        }
-
-        files.forEach((e) => {
-          const sourceFile = path.join(source, e.name);
-          const destFile = path.join(dest, e.name);
-
-          if (e.isDirectory()) {
-            copyDir(sourceFile, destFile);
-          } else {
-            filesys.copyFile(sourceFile, destFile, (err) => {
-              if (err) {
-                throw err;
-              }
-            });
+  doesExist(dest, (err, exist) => {
+    if (exist) {
+      filesys.rm(
+        dest,
+        {
+          recursive: true,
+          force: true,
+        },
+        (err) => {
+          if (err) {
+            throw err;
           }
+          copyDir(source, dest);
+        }
+      );
+    } else {
+      filesys.mkdir(dest, { recursive: true }, (err) => {
+        if (err) {
+          throw err;
+        }
+
+        filesys.readdir(source, { withFileTypes: true }, (err, files) => {
+          if (err) {
+            throw err;
+          }
+
+          files.forEach((e) => {
+            const sourceFile = path.join(source, e.name);
+            const destFile = path.join(dest, e.name);
+
+            if (e.isDirectory()) {
+              copyDir(sourceFile, destFile);
+            } else {
+              filesys.copyFile(sourceFile, destFile, (err) => {
+                if (err) {
+                  throw err;
+                }
+              });
+            }
+          });
         });
       });
-    });
-  }
+    }
+  });
 }
 
-function doesExist(path) {
-  try {
-    const stats = filesys.statSync(path);
-    return stats.isDirectory();
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      return false;
+function doesExist(path, callback) {
+  filesys.stat(path, (err, stats) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        callback(false);
+      } else {
+        callback(err);
+      }
     } else {
-      throw err;
+      callback(null, stats.isDirectory());
     }
-  }
+  });
 }
 
 copyDir(source, dest);
